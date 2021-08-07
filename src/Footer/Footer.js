6e3cea82316548e7657d7ebe520ee3a360348714
Sparsh101AI <sparshpatel6165@gmail.com>
2021-08-07 11:54:34 -0600

    added update

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,4 +1,4 @@
-import './Footer';
+import './Footer.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import './Navbar';
+import './Navbar.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
