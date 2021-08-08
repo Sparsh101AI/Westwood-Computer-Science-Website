40dd9240914ffc2c339dba1cfd89d6808a2b73b7
Sparsh101AI <sparshpatel6165@gmail.com>
2021-08-08 14:07:35 -0600

    links for socials added

--- src/SocialFollow.js
+++ src/SocialFollow.js
@@ -12,22 +12,22 @@ export default function SocialFollow() {
     <div className="social-container">
     
       <a
-        href="https://www.youtube.com/c/jamesqquick"
+        href="https://www.youtube.com/channel/UC9dw1djZTPJTC3Hn8dBxzVg"
         className="youtube social"
       >
         <FontAwesomeIcon icon={faYoutube} size="2x" />
       </a>
       <a
-        href="https://www.facebook.com/learnbuildteach/"
+        href="https://www.facebook.com/wwhighschool/"
         className="facebook social"
       >
         <FontAwesomeIcon icon={faFacebook} size="2x" />
       </a>
-      <a href="https://www.twitter.com/jamesqquick" className="twitter social">
+      <a href="https://twitter.com/InovWw" className="twitter social">
         <FontAwesomeIcon icon={faTwitter} size="2x" />
       </a>
       <a
-        href="https://www.instagram.com/learnbuildteach"
+        href="https://www.instagram.com/wwhighschool/"
         className="instagram social"
       >
         <FontAwesomeIcon icon={faInstagram} size="2x" />
