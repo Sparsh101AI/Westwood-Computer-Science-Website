e119e27f6a9b32162b02445bde8d65490fa9fed5
Sparsh101AI <sparshpatel6165@gmail.com>
2021-08-20 13:06:29 -0600

    finsihed navbar

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -7,39 +7,48 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Button from 'react-bootstrap/Button'
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
-
+import Typical from 'react-typical';
 function bar() {
     return (
+      
+      
+      
+      
       <div className="bar">
-  <Navbar className="nav-colortop" >
-
+ 
+ {/*top navbar */}
+ <Navbar bg="light" >
+    <Container>
       <Navbar.Brand href="#home">
         <img
           alt=""
-          src="/mlogo.png"
-          width="70"
+          src="capture.png"
+          width="170"
           height="70"
-         
+          className="d-inline-block align-top"
         />{' '}
-     Header
-      </Navbar.Brand>
+        </Navbar.Brand>
+         <Navbar.Toggle />
+     <Navbar.Collapse className="justify-content-end">
+      </Navbar.Collapse>
+    </Container>
   </Navbar>
 
+ {/*middle navbar */}
 
   <Navbar collapseOnSelect expand="lg" className="nav-color" variant="dark">
     
   <Container>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="me-auto">
+    <Nav className="auto">
      
-    <Button className="button-color" href="#">About</Button> 
-    <Button className="button-color" href="#">CS Junior High</Button> 
-    <Button className="button-color" href="#">CS Senior High</Button> 
-    <Button className="button-color" href="#">Robotics Program</Button> 
+    <Button className="button-color"   href="#">About</Button> 
+    <Button className="button-color" href="#">Computer Science</Button> 
+    <Button className="button-color" href="#">Robotics</Button> 
+    <Button className="button-color" href="#">Digital Design</Button> 
     <Button className="button-color" href="#">Awards and Achievements
 </Button> 
-
 
     </Nav>
    
@@ -49,16 +58,28 @@ function bar() {
 
 
 
+ {/*bottom navbar */}
 
 
+<Navbar className="midbar">
+<p className="navtex"> Welcome To: {' '}
 
-
-
-
-
-
-
-
+<Typical
+  loop={Infinity}
+  wrapper="b"
+  steps={
+    [
+      'Innovation',
+      1400,
+      'Creativity',
+      1400,
+      'Our Community',
+      1400,
+    ]
+  }
+/>
+</p>
+</Navbar>
       </div>
     );
   }
