df013b5ccc6e70e0fe9d6f16edbb426a38770818
Sparsh101AI <sparshpatel6165@gmail.com>
2021-09-04 13:25:44 -0600

    added packages

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,5 +1,4 @@
 import './Home.css';
-import Carousel from 'react-bootstrap/Carousel'
 import Row from 'react-bootstrap/Row'
 import Card from 'react-bootstrap/Card'
 import Col from 'react-bootstrap/Col'
@@ -18,127 +17,22 @@ function Home() {
   <Row>
     <Col >
       
-    <Carousel className="cards3">
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="wwlogo.png"      alt="First slide"
-    />
-    <Carousel.Caption>
-      
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="wwlogo.png"
-      alt="Second slide"
-    />
-
-    <Carousel.Caption>
-
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="wwlogo.png"      alt="Third slide"
-    />
-
-    <Carousel.Caption>
-     
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-
-
-
-
-
-
-
-
-
-
+   
 
     </Col>
-    {''}
-    <Col sm={4}>sm=4</Col>
+    
+  
   </Row>
 
 
 
 <br/>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 
 
 
 
-
-
-<Row>
-
-  
-        
-
-
-  <Col  xl={6}>
-  <Card className="videopart" >
-  <Card.Body>
-    <Card.Title className="videopart">Meet Our Teachers</Card.Title>
-    <Card.Text>
-    <CardGroup>
- 
- <Card className="teacherpart1and2">
-   <Card.Img variant="top" src="saidheadimg.png" />
-   <Card.Body>
-     <Card.Title>Said El Medjdani</Card.Title>
-     <Card.Text>
-     "As a life-long learner, I believe that students grow up with challenges, computational thinking,
-      problem solving and product-oriented learning. As a father of three, one of my greatest passions in life
-       is to see kids grow and learn."{' '}
-     </Card.Text>
-   </Card.Body>
-   
- </Card>
- <Card className="teacherpart1and2">
-   <Card.Img variant="top" src="saidheadimg.png" />
-   <Card.Body>
-     <Card.Title>Jeff Landry</Card.Title>
-     <Card.Text>
-       "This is a wider card with supporting text below as a natural lead-in to
-       additional content. This card has even longer content than the first to
-       show that equal height action."
-     </Card.Text>
-   </Card.Body>
-  
- </Card>
-</CardGroup>
-    </Card.Text>
-  </Card.Body>
-</Card>
-
-  
-
-  </Col>
-
-
-
-
-
-
-
-
-
-
-        
-    
-  
-
-
-
-</Row>
-
-<br/>
 
 <Row>
   {/*This is westwood centre video */}
